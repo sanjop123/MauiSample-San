cff3435bfdabcabfe01ff591826d1c6be681d58d
sanjop123 <94389225+sanjop123@users.noreply.github.com>
2023-10-26 14:15:27 +0100

    Add files via upload

---
+++ Personal-Service/SubmitPersonalInformationRequest.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using personal_data;
+namespace Personal_Service
+{
+    public class SubmitPersonalInformationRequest : IRequest<bool>
+    {
+        public PersonalInfo personalInfo { get; set; }  
+    }
+
+   public class SubmitPersonInforamtionHandler : IRequestHandler<SubmitPersonalInformationRequest,bool>
+    {
+        public Task<bool> Handle (SubmitPersonalInformationRequest request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult (true);
+        }
+
+    }
+
+}
---
+++ personal-data/PersonalInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace personal_data
+{
+    public class PersonalInfo
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+    }
+}
